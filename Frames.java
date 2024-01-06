44ea4577fa183fb0263eb34aec9cae47e1cbea3b
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-01-06 11:54:28 +0530

    import java.util.*; public class Sm{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int k=(a+b)*2;         System.out.println(k*c);     } }

---
+++ Frames.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sm{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int k=(a+b)*2;
+        System.out.println(k*c);
+    }
+}
