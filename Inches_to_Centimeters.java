766c60a9789e517ab90c91a71f1dbe80dd022989
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-01-07 16:27:08 +0530

    import java.util.*; public class Sm{     public static void main(String args[])     {         Scanner k=new Scanner(System.in);         double a= k.nextInt();         System.out.printf("%.2f",(2.54)*a);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sm{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+        double a= k.nextInt();
+        System.out.printf("%.2f",(2.54)*a);
+    }
+}
