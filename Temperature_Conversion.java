42e44418fa196a431f0e7eca3d44c0bc56a97805
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-01-07 16:04:32 +0530

    import java.util.*; public class Sm{     public static void main(String args[])     {         Scanner k=new Scanner(System.in);         double c= k.nextInt();         double s=c*1.8;         double a=s+32;         System.out.printf("%.2f",a);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sm{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+        double c= k.nextInt();
+        double s=c*1.8;
+        double a=s+32;
+        System.out.printf("%.2f",a);
+    }
+}
