3aaddc5467136e27c6d5e716d2acd54fd0f31173
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-01-07 16:49:08 +0530

    import java.util.*; public class Sm{     public static void main(String args[])     {         Scanner k=new Scanner(System.in);          int a= k.nextInt();         double s= (5.0/18.0)*a;         System.out.printf("%.2f",s);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sm{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+         int a= k.nextInt();
+        double s= (5.0/18.0)*a;
+        System.out.printf("%.2f",s);
+    }
+}
