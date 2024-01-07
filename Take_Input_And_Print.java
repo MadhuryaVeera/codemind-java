d6b8338b53b323a02badc74e8a40c456ee987d41
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-01-07 15:36:49 +0530

    import java.util.*; public class Sm{     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String  a= s.nextLine();         //int b= s.nextInt();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sm{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String  a= s.nextLine();
+        //int b= s.nextInt();
+        System.out.println(a);
+    }
+}
