f771aa4b57798aff0e19294b56d6e58da330146f
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-02-13 22:29:28 +0530

    import java.util.Scanner; public class Ex{   public static void main(String args[]){       Scanner sc=new Scanner(System.in);       //reading the string input with the spaces       String n= sc.nextLine();       // copying the string to another string by changing the string varaible naem       String nn= n;       System.out.println(nn);
       }
    }

---
+++ String_Copy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ex{
+  public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      //reading the string input with the spaces
+      String n= sc.nextLine();
+      // copying the string to another string by changing the string varaible naem
+      String nn= n;
+      System.out.println(nn);
+      
+   }
+}
