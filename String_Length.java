6fe518e27aaae404059abe324d8029dac9cf89ce
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-02-13 23:48:40 +0530

    import java.util.Scanner; public class Ex{   public static void main(String args[]){       Scanner sc=new Scanner(System.in);       String n= sc.next();        System.out.print(n.length());    } }

---
+++ String_Length.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Ex{
+  public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      String n= sc.next();
+       System.out.print(n.length());
+   }
+}
