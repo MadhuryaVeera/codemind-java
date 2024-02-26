a1debf55b879768bcf9c65390ffe2fb60eb9bb01
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-02-26 20:18:16 +0530

    import java.util.*; public class LowerCase{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         String  s  = sc.nextLine();  // reading input String         System.out.println(s.toLowerCase());  // toLowerCase is used in java with a camel case to change upper to lower case letters.     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class LowerCase{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);  
+        String  s  = sc.nextLine();  // reading input String 
+        System.out.println(s.toLowerCase());  // toLowerCase is used in java with a camel case to change upper to lower case letters.
+    }
+}
