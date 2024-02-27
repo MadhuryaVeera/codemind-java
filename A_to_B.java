7081010540fccdf75854b2ef67aa57d4320b8eff
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-02-27 21:04:44 +0530

    import java.util.*; public class VowelConst{     public static void main(String args[]){         Scanner sc= new Scanner(System.in);         int n = sc.nextInt();         int y = sc.nextInt();         for(int i = n; i<=y; i++){             System.out.print(i+" ");         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class VowelConst{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int y = sc.nextInt();
+        for(int i = n; i<=y; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
