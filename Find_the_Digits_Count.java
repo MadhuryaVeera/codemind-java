985afb6837105bca2609858c1012bba068d9b2b9
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-02-27 20:29:08 +0530

    import java.util.*; public class DigitCount{     public static void main(String args[]){      Scanner sc = new Scanner(System.in);      int n = sc.nextInt();      String s1=String.valueOf(n);      System.out.println(s1.length());     } }

--- Find_the_Digits_Count.java
+++ Find_the_Digits_Count.java
@@ -1,14 +1,9 @@
 import java.util.*;
 public class DigitCount{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();  //123
-        int c=0;  
-        while(n!=0)   // 123!=0
-        {
-            n=n/10;   //123/10=12; 12/10=1; 1/10=0
-            c++;      // 1,2,3
-        }
-        System.out.println(c);
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     String s1=String.valueOf(n);
+     System.out.println(s1.length());
     }
 }
