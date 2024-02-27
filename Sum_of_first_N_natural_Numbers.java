48296db25fd4d2c7c86c635828d1ae34e44f8075
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-02-27 06:02:16 +0530

    import java.util.*; public class Nat{     public static void main(String aargs[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         System.out.println(n*(n+1)/2);     } }

---
+++ Sum_of_first_N_natural_Numbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Nat{
+    public static void main(String aargs[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
